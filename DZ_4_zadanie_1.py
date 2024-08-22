abcd2ccf9334d8ab3187896c24b33b7037ca8c8b
LyamzaevS <tj-perm@mail.ru>
2024-08-23 00:02:48 +0500

    Test

---
+++ DZ_4_zadanie_1.py
@@ -0,0 +1,6 @@
+"""Задание 1.
+Выведите на экран надпись To be or not to be на разных строках.
+Пример вывода: To be
+ or not
+ to be"""
+print('to be\nor not\nto be')
---
+++ DZ_4_zadanie_2.py
@@ -0,0 +1,8 @@
+"""Задание 2.
+Выведите на экран надпись «Life is what happens when you're busy making other
+plans» John Lennon на разных строках.
+Пример вывода:
+ “Life is what happens when
+ you’re busy making
+ other plans” John Lennon."""
+print('Life is what happens when\n    you’re busy making\n        other plans” John Lennon.')
