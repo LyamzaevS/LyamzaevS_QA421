2ebf1936c1dea186d51e966f832269af8bf8524b
LyamzaevS <tj-perm@mail.ru>
2024-08-27 22:06:04 +0500

    Test

---
+++ DZ_4.py
@@ -0,0 +1,33 @@
+"""Задание 1.
+Выведите на экран надпись To be or not to be на разных строках.
+Пример вывода: To be
+ or not
+ to be"""
+print('to be\nor not\nto be')
+print()
+"""Задание 2.
+Выведите на экран надпись «Life is what happens when you're busy making other
+plans» John Lennon на разных строках.
+Пример вывода:
+ “Life is what happens when
+ you’re busy making
+ other plans” John Lennon."""
+print('Life is what happens when\n    you’re busy making\n        other plans” John Lennon.')
+print()
+"""Задание 3.
+Определите тип данных 10 и '10'
+Результат выведите на экран"""
+print()
+print(type('10'))
+print(type(10))
+print()
+"""Задание 4.
+Выведите на экран треугольник из звездочек
+Пример вывода:
+*
+**
+***
+****
+*****"""
+print()
+print('*\n**\n***\n****\n*****')
